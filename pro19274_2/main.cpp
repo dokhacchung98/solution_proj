4b984df876a844053c3dccc45987781859803d70
khacchungory <ochung3@gmail.com>
2020-05-15 15:19:59 +0700

    19274-Operators

--- pro19274_2/main.cpp
+++ pro19274_2/main.cpp
@@ -1,52 +1,22 @@
-#include <iostream>
-
-using namespace std;
-int arr[500];
-int n, s;
-int check = 0;
-int tong(){
-    int tmp = 0;
-    for(int i = 0; i < n; i++){
-        tmp += arr[i];
-    }
-    return tmp;
+#include<stdio.h>
+bool Check(long L,long R,long S)
+{
+    if(S>R || S<L || S==R-2 || S==L+2) return false;
+    return true;
 }
-void changeOperator(int index){
-    if(check == 0){
-        arr[index] *= -1;
-        int s1 = tong();
-        if(s1 == s){
-            check = 1;
-            for(int i = 0; i < n; i++){
-                if(arr[i]>0 && i > 0){
-                    cout << "+" << arr[i];
-                }else {
-                    cout << arr[i];
-                }
-            }
-            return;
-        }
-        if(index < n - 1){
-            changeOperator(index + 1);
-            arr[index] *= -1;
-            changeOperator(index + 1);
-        }
-    }
+void Print(long L,long R,long n,long S)
+{
+    if(n==1) {printf("1");return;}
+    if(Check(L+n,R-n,S-n)) {Print(L+n,R-n,n-1,S-n);printf("+%ld",n);}
+    else {Print(L+n,R-n,n-1,S+n);printf("-%ld",n);}
 }
 int main()
 {
-    cin >> n;
-    cin >> s;
-    for(int i = 0; i < n; i++){
-        arr[i] = i + 1;
-    }
-    if(tong() < s){
-        cout << "Impossible";
-    }
-    changeOperator(1);
-
-    if(check == 0){
-        cout << "Impossible";
-    }
-    return 0;
+    long n,S,L,R;
+    scanf("%ld%ld",&n,&S);
+    R=(n+1)*n/2;
+    L=2-R;
+    if((S+R)%2) printf("Impossible");
+    else if(!Check(L,R,S)) printf("Impossible");
+    else Print(L,R,n,S);
 }
