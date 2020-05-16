d4ab8eb6f78ce538f66c2c0a36c1df05fa7b8da7
khacchungory <ochung3@gmail.com>
2020-05-16 11:59:15 +0700

    297-Aggressive cows

--- pro297_2/main.cpp
+++ pro297_2/main.cpp
@@ -1,28 +1,54 @@
 #include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
-long int n;
-int t;
-int c;
-long long int arr[100000];
-int result = 0;
+int n,c;
+int func(int num,int arr[])
+{
+    int cows=1;
+    int pos=arr[0];
+    for (int i=1; i<n; i++)
+    {
+        if (arr[i]-pos>=num)
+        {
+            pos=arr[i];
+            cows++;
+            if (cows==c)
+                return 1;
+        }
+    }
+    return 0;
+}
+int bs(int arr[])
+{
+    int ini=0,last=arr[n-1],max=-1;
+    while (last>ini)
+    {
+        int mid=(ini+last)/2;
+        if (func(mid,arr)==1)
+        {
+            if (mid>max)
+                max=mid;
+            ini=mid+1;
+        }
+        else
+            last=mid;
+    }
+    return max;
+}
 int main()
 {
+    int t;
     cin >> t;
-    while(t > 0){
+    while (t > 0)
+    {
         cin >> n >> c;
-        for(int i = 0; i < n; i++){
+        int arr[n];
+        for (int i=0; i<n; i++)
             cin >> arr[i];
-        }
-        sort(arr, arr+n);
-        for(int i = 0; i < n; i++){
-            cout<<arr[i]<<" ";
-        }
-        while(1){
-
-            result++;
-        }
+        sort(arr,arr+n);
+        int result = bs(arr);
+        cout << result << endl;
         t--;
     }
     return 0;
