67707aec27aa1234155507a7043161240fa9d002
khacchungory <ochung3@gmail.com>
2020-05-25 23:25:34 +0700

    10283-ALL IZZ WELL

--- pro10283_2/main.cpp
+++ pro10283_2/main.cpp
@@ -1,74 +1,72 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-int t, h, w;
-char arr[102][102];
-int arrTmp[102][102];
-int arrSXA[10];
-int arrSYA[10];
-int numberSA = 0;
-int arrSXI[10];
-int arrSYI[10];
-int numberSI = 0;
-int arrSXW[10];
-int arrSYW[10];
-int numberSW = 0;
-int check = 0;
-char tmp[] = {'L','L','I','Z','Z','W','E','L','L'};
-int tmpIndex = 0;
-void tryMove(int y, int x){
-    if(check == 1){
+
+const char arrTmp[] = {'A', 'L','L','I','Z','Z','W','E','L','L'};
+
+int  h, w;
+int  ans;
+char arr[105][105];
+int tmp[105][105];
+
+int drow[8] = {-1,-1,0,1,1, 1, 0,-1};
+int dcol[8] = { 0, 1,1,1,0,-1,-1,-1};
+
+void tryMove(int row, int col, int index)
+{
+    if(ans == 1){
         return;
     }
+	if(index == 9)
+	{
+		ans = 1;
+		return;
+	}
 
+	for(int i = 0; i < 8; i++)
+	{
+		int r = row + drow[i];
+		int c = col + dcol[i];
+
+		if (r >= 0 && r < h && c >= 0 && c < w &&
+			!tmp[r][c] && arr[r][c] == arrTmp[index+1])
+		{
+			tmp[r][c] = 1;
+			tryMove(r, c, index+1);
+			tmp[r][c] = 0;
+		}
+	}
 }
+
 int main()
 {
-    cin >> t;
-    while(t > 0){
-        numberS = 0;
-        tmpIndex = 0;
-        cin >> h >> w;
-        for(int i = 0; i < w + 2; i++){
-            arrTmp[0][i] = 1;
-            arrTmp[h+1][i] = 1;
-        }
-        for(int i = 1; i < h + 1; i++){
-            arrTmp[i][0] = 1;
-            arrTmp[i][w+1] = 1;
-        }
-        for(int i = 1; i <= h; i++){
-            for(int j = 1; j <= w; j++){
+	int t;
+	cin >> t;
+	for(int tc = 0; tc < t; tc++)
+	{
+		ans = 0;
+		cin >> h >> w;
+		for(int i = 0; i < h; i++){
+            for(int j = 0; j < w; j++){
                 cin >> arr[i][j];
-                arrTmp[i][j] = 0;
-                if(arr[i][j] == 'A'){
-                    arrSX[numberS] = j;
-                    arrSY[numberS] = i;
-                    numberS++;
-                }
+                tmp[i][j] = false;
             }
-        }
-        for(int i = 0; i < h+2; i++){
-            for(int j = 0; j < w+2; j++){
-                cout << arrTmp[i][j]<< " ";
-            }
-            cout << endl;
-        }
-        if(numberS == 0){
-            printf("NO");
-        }else{
-            for(int i = 0; i < numberS; i++){
-                arrTmp[arrSY[i]][arrSX[i]] = 1;
-                tryMove(arrSY[i], arrSX[i]);
-                arrTmp[arrSY[i]][arrSX[i]] = 0;
-            }
-            if(check == 0){
-                cout << "NO" << endl;
-            }else{
-                cout << "YES" << endl;
-            }
-        }
-        t--;
-    }
-    return 0;
+		}
+
+		for(int i = 0; i < h; i++)
+		{
+			for(int j = 0; j < w; j++)
+				if(arr[i][j] == 'A')
+				{
+					tmp[i][j] = 1;
+					tryMove(i, j, 0);
+					tmp[i][j] = 0;
+
+					if(ans) break;
+				}
+
+			if(ans) break;
+		}
+		if(ans == 1) cout << "YES" << endl;
+		else cout << "NO" << endl;
+	}
 }
---
+++ pro372_2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+int t;
+int n;
+ckass Edge{
+public:
+    int dau, cuoi;
+    long d;
+}
+long tmp[50005];
+Edge arr[50005];
+
+int main()
+{
+    cin >> t;
+    while(t > 0){
+        cin >> n;
+        int tmp1, tmp2;
+        for(int i = 0; i < n; i++){
+
+        }
+        t--;
+    }
+    return 0;
+}
