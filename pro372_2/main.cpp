e1f151dd9e5823942886550adc2bd25029a23e74
khacchungory <ochung3@gmail.com>
2020-05-28 12:57:18 +0700

    43-Copying Books

--- pro372_2/main.cpp
+++ pro372_2/main.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 int t;
 int n;
+int arr
 ckass Edge{
 public:
     int dau, cuoi;
     long d;
 }
-long tmp[50005];
+long tmp[50005][50005];
 Edge arr[50005];
 
 int main()
@@ -16,8 +17,9 @@ int main()
     cin >> t;
     while(t > 0){
         cin >> n;
-        int tmp1, tmp2;
+        int tmp1, tmp2, v;
         for(int i = 0; i < n; i++){
+            cin >> tmp1 >> tmp2 >> v;
 
         }
         t--;
--- pro43_2/main.cpp
+++ pro43_2/main.cpp
@@ -1,13 +1,78 @@
 #include <iostream>
 
 using namespace std;
-
+int n, t, m;
+int arr[505];
+int tmps[505];
+int tmpe[505];
+void update(int x, long mid){
+    long s = 0;
+    int k = tmps[x];
+    for(long i = tmps[x+1] - 1; i >= k; i--){
+        s += arr[i];
+        if(s == mid){
+            tmps[x] = i;
+            break;
+        }else if(s > mid){
+            tmps[x] = i + 1;
+            break;
+        }
+    }
+}
+int isValid(long mid){
+    for(int i = 0; i < m; i++){
+        tmps[i] = i;
+    }
+    tmps[m] = n;
+    for(int i = m - 1; i >= 0; i--){
+        update(i, mid);
+    }
+    if(tmps[0] > 0){
+        return 0;
+    }
+    return 1;
+}
 int main()
 {
-    int n, t;
     cin >> t;
     while(t > 0){
-
+        cin >> n >> m;
+        long maxPage = 0;
+        long sumPage = 0;
+        for(int i = 0; i < n; i++){
+            cin >> arr[i];
+            if(arr[i] > maxPage){
+                maxPage = arr[i];
+            }
+            sumPage += arr[i];
+        }
+        long l = maxPage;
+        long r = sumPage;
+        while(l < r){
+            long mid = (l + r)/2;
+            if(isValid(mid) == 1){
+                r = mid;
+                for(int i = 0; i < m; i++){
+                    tmpe[i] = tmps[i];
+                }
+            }else{
+                l = mid + 1;
+            }
+        }
+        for(int i = 0; i < m - 1; i++){
+            for(int j = tmpe[i]; j < tmpe[i+1]; j++){
+                cout << arr[j] << " ";
+            }
+            cout << "/ ";
+        }
+        for(int i = tmpe[m-1]; i < n;  i++){
+            cout << arr[i];
+            if(i == n - 1){
+                break;
+            }
+            cout << " ";
+        }
+        cout << endl;
         t--;
     }
     return 0;
